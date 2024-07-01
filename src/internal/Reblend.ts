e89d745a8020441d81d2c8817743497d870270ad
Emmanuel Paul Elom <scansioquielom@gmail.com>
2024-07-01 23:41:44 +0100

    fix

--- src/internal/Reblend.ts
+++ src/internal/Reblend.ts
@@ -1,6 +1,27 @@
 import { registerElement } from "../common/utils";
 import BaseComponent from "./BaseComponent";
 
+declare global {
+  namespace JSX {
+    namespace JSX {
+      type ElementType = ReblendTyping.JSX.ElementType;
+      interface Element extends ReblendTyping.JSX.Element {}
+      interface ElementClass extends ReblendTyping.JSX.ElementClass {}
+      interface ElementAttributesProperty
+        extends ReblendTyping.JSX.ElementAttributesProperty {}
+      interface ElementChildrenAttribute
+        extends ReblendTyping.JSX.ElementChildrenAttribute {}
+      type LibraryManagedAttributes<C, P> =
+        ReblendTyping.JSX.LibraryManagedAttributes<C, P>;
+      interface IntrinsicAttributes
+        extends ReblendTyping.JSX.IntrinsicAttributes {}
+      interface IntrinsicClassAttributes<T>
+        extends ReblendTyping.JSX.IntrinsicClassAttributes<T> {}
+      interface IntrinsicElements extends ReblendTyping.JSX.IntrinsicElements {}
+    }
+  }
+}
+
 class Reblend extends BaseComponent {
   static ELEMENT_NAME = "Fragment";
 
