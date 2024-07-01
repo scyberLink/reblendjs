2a4ddfc73cdf24a11de353612f620ff27579a593
Emmanuel Paul Elom <scansioquielom@gmail.com>
2024-07-01 18:59:29 +0100

    Improve JSX typings

--- src/internal/Reblend.ts
+++ src/internal/Reblend.ts
@@ -1,27 +1,6 @@
 import { registerElement } from "../common/utils";
 import BaseComponent from "./BaseComponent";
 
-declare global {
-  namespace JSX {
-    interface Element extends HTMLElement {}
-
-    interface IntrinsicElements extends IntrinsicElementMap {}
-
-    type IntrinsicElementMap = {
-      [K in keyof HTMLElementTagNameMap]: HTMLElement;
-    };
-
-    /* interface Component {
-      (properties?: { [key: string]: any }, children?: Node[]): HTMLElement;
-    } */
-
-    interface ElementClass extends Reblend {}
-    interface ElementAttributesProperty {
-      props: {};
-    }
-  }
-}
-
 class Reblend extends BaseComponent {
   static ELEMENT_NAME = "Fragment";
 
